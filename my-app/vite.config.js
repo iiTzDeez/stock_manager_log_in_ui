0f6533785584bfd9f7bba52db361a292e8ae9085
iiTzDeez <goncalomfvn@gmail.com>
2025-09-26 01:03:04 +0100

    .

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/stock_manager_log_in_ui/', // tem de ser o nome do teu repositório no GitHub
 })
